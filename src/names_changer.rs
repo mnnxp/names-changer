16c8c341633c293ed0595cd5579c9a61b00fa883
I_Nosovskiy <mananaxep@gmail.com>
2021-01-30 12:52:13 +0000

    have bugs, change dependencies case to heck

--- src/names_changer.rs
+++ src/names_changer.rs
@@ -1,4 +1,4 @@
-use case::CaseExt;
+use heck::SnakeCase;
 use regex::Regex;
 
 pub trait NamesChanger {
@@ -11,7 +11,8 @@ pub trait NamesChanger {
     /// ```
     /// use names_changer::NamesChanger;
     ///
-    /// assert_eq!(&"bus WildUnderscoreS oby martinLutherStringJr 言語".camel_to_snake(), "bus wild_underscore_s oby martin_luther_string_jr 言語");
+    /// assert_eq!(&"bus WildUnderscoreS oby martinLutherStringJr 言語".camel_to_snake(),
+    /// "bus wild_underscore_s oby martin_luther_string_jr 言語");
     /// ```
     fn camel_to_snake(&self) -> Self::Owned;
 }
@@ -21,13 +22,18 @@ impl NamesChanger for str {
 
     fn camel_to_snake(&self) -> String {
         let re_word_for_change =
-            Regex::new(r"([a-z]?[a-z]*[A-Z]+[a-z]+([A-Z]?[a-z]*[\w]){0,10})")
-                .unwrap();
-        let re_word_with_abbreviation =
-            Regex::new(r"(\w(?<=[^a-z])(?<=[A-Z].)[A-Z]+)")
+            Regex::new(r"([a-z]?[a-z]*[A-Z]+[a-z]+[A-Z]?[a-zA-Z0-9-]+[^\W])")
                 .unwrap();
         let re_word_to_word =
-            Regex::new(r"(?<=[a-zA-Z])([2][_](?=\w))")
+            //Regex::new(r"([\w][2][a-zA-Z])")
+            //Regex::new(r"([a-z])([2][_])([a-z])")
+            Regex::new(r"(?P<word1>[a-z]+)(?P<to1>[2])(?P<word2>[a-z_]+)")
+                .unwrap();
+        let re_custom_word =
+            Regex::new(r"(?P<nword1>[\W]*)(?P<word1>[a-zA-Z0-9-_]+[a-z]?[a-z]*[A-Z]*[a-z]+[A-Z]?[a-zA-Z0-9-_]*)(?P<nword2>[\W]*)")
+                .unwrap();
+        let re_non_any_word =
+            Regex::new(r"([^a-zA-Z0-9-_])")
                 .unwrap();
         let mut result = Vec::new();
 
@@ -43,13 +49,27 @@ impl NamesChanger for str {
                 if re_word_for_change.is_match(word) {
                     change_word = word.parse().unwrap();
 
-
-                    change_word = change_word.to_snake();
+                    if re_non_any_word.is_match(&change_word) {
+                        let rep_word = &re_custom_word.replace(&change_word.as_str(), "$word1");
+                        let update_word = format!("{} {}{}", "$nword1", rep_word.to_snake_case(), "$nword2");
+                        let new_word = &re_custom_word.replace(
+                            &change_word.as_str(),
+                            update_word.as_str());
+                        let new_word = new_word.replace(" ", "");
+                        change_word = new_word.to_string();
+                    } else {
+                        change_word = change_word.to_snake_case();
+                    }
 
                     // searching "2_" in word received of word "Text2Text"
                     // for change to "text_to_text"
                     if re_word_to_word.is_match(&change_word) {
-                        change_word = change_word.replace("2_", "_to_");
+                        //let rep_word = &re_word_to_word.replace(&change_word.as_str(), "$to1");
+                        //change_word = change_word.replace("2_", "_to_");
+                        let new_word = &re_word_to_word.replace(&change_word,
+                                            format!("{} _to{}", "$word1", "$word2").as_str());
+                        let new_word = new_word.replace(" ", "");
+                        change_word = new_word.to_string();
                     }
                     //println!("TRUE: {}", w);
 
--- tests/module.rs
+++ tests/module.rs
@@ -2,7 +2,7 @@
 mod tests {
     use names_changer::*;
     #[test]
-    fn test_name_change() {
+    fn check_classic_camel_word_change() {
         let content = "TABLE ClientTokensRef IS 'text';";
         let change_content = content.camel_to_snake();
 
@@ -10,27 +10,60 @@ mod tests {
     }
 
     #[test]
-    fn check_return_data() {
+    fn check_change_word_with_abbreviation() {
+        let content = "CONForSTRChangerRAINT TABLE idNameCAD IS 'text';";
+        let change_content = content.camel_to_snake();
+
+        assert_eq!("con_for_str_changer_raint TABLE id_name_cad IS 'text';", change_content)
+    }
+
+    #[test]
+    fn check_change_word_with_to() {
+        let content = "TABLE Client2KentsRef IS 'text';";
+        let change_content = content.camel_to_snake();
+
+        assert_eq!("TABLE client_to_kents_ref IS 'text';", change_content)
+    }
+
+    #[test]
+    fn check_change_word_with_abbreviation_and_to() {
+        let content = "TABLE Name-CAD Client2CADRef IS (idCADNameCAD!) 'text';";
+        let change_content = content.camel_to_snake();
+
+        assert_eq!("TABLE name_cad client_to_cad_ref IS (id_cad_name_cad!) 'text';", change_content)
+    }
+
+
+    #[test]
+    fn check_change_word_with_abbreviation_and_other_chars() {
+        let content = "ALTER TABLE FileRef @idCADNameCAD@ ADD CONSTRAINT FileRef_fk1 FOREIGN KEY (idClientCreate) REFERENCES";
+        let change_content = content.camel_to_snake();
+
+        assert_eq!("ALTER TABLE file_ref @id_cad_name_cad@ ADD CONSTRAINT file_ref_fk1 FOREIGN KEY (id_client_create) REFERENCES", change_content)
+    }
+
+    #[test]
+    fn check_change_full_data() {
         let contents = "\
 /* перечень типов изменений */
-CREATE TABLE TypeOfChangeRef (
+CREATE TABLE TypeOfChangeRef2 (
   id integer NOT NULL, /* id типа изменения */
-  TypeOfChange VARCHAR(100) NOT NULL, /*наименование изменения */
+  2_TypeOfChange VARCHAR(100) NOT NULL, /*наименование изменения */
   idNameCAD integer NOT NULL DEFAULT '0', /* САПР «по умолчанию» (для быстрой загрузки данных) */
   CONSTRAINT TypeOfChangeRef_pk PRIMARY KEY (id)
 );
-ALTER TABLE FileRef ADD CONSTRAINT FileRef_fk1 FOREIGN KEY (idClientCreate) REFERENCES ClientRef(id);
-COMMENT ON TABLE Business2Client IS 'commented';";
+ALTER TABLE FileRef @idCADNameCAD@ ADD CONSTRAINT FileRef_fk1 FOREIGN KEY (idClientCreate) REFERENCES ClientRef(id);
+COMMENT (idCADNameCAD) ON TABLE Business2Client IS 'commented';";
         let good_content = "\
 /* перечень типов изменений */
-CREATE TABLE type_of_change_ref (
+CREATE TABLE type_of_change_ref2 (
   id integer NOT NULL, /* id типа изменения */
-  type_of_change VARCHAR(100) NOT NULL, /*наименование изменения */
+  2_type_of_change VARCHAR(100) NOT NULL, /*наименование изменения */
   id_name_cad integer NOT NULL DEFAULT '0', /* САПР «по умолчанию» (для быстрой загрузки данных) */
   CONSTRAINT type_of_change_ref_pk PRIMARY KEY (id)
 );
-ALTER TABLE file_ref ADD CONSTRAINT file_ref_fk1 FOREIGN KEY (id_client_create) REFERENCES client_ref(id);
-COMMENT ON TABLE business_to_client IS 'commented';";
+ALTER TABLE file_ref @id_cad_name_cad@ ADD CONSTRAINT file_ref_fk1 FOREIGN KEY (id_client_create) REFERENCES client_ref(id);
+COMMENT (id_cad_name_cad) ON TABLE business_to_client IS 'commented';";
 
 
         assert_eq!(good_content, NamesChanger::camel_to_snake(contents));
